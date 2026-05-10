18a5f74957166ebd0681d0b2e04bb5d301b2cf4f
Rustam <rustamsharay@gmail.com>
2026-05-10 23:05:47 +0300

    Add homework 3

---
+++ decision-tree.js
@@ -0,0 +1,14 @@
+const age = 20;
+const hasPassport = true;
+
+if (age >= 18 && hasPassport) {
+  console.log('Access granted');
+}
+
+if (age < 18) {
+  console.log('You are under 18');
+} else if (age >= 18 && hasPassport) {
+  console.log('You can travel');
+} else {
+  console.log('Passport required');
+}
---
+++ switch.js
@@ -0,0 +1,19 @@
+const day = 'Monday';
+
+switch (day) {
+  case 'Monday':
+    console.log('Start of the work week');
+    break;
+
+  case 'Friday':
+    console.log('Almost weekend');
+    break;
+
+  case 'Saturday':
+  case 'Sunday':
+    console.log('Weekend');
+    break;
+
+  default:
+    console.log('Regular day');
+}
