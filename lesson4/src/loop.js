55d6d391b3db5e04340e44b7e02ed2f5086a3565
Rustam <rustamsharay@gmail.com>
2026-05-18 12:47:33 +0300

    fixed homework issues

--- lesson4/src/loop.js
+++ lesson4/src/loop.js
@@ -5,3 +5,31 @@ for (let i = 0; i <= '9'; i++) {
 for (let i = 100; i >= 0; i -= 10) {
     console.log(i);
 }
+
+let i = 0;
+
+while (i < 10) {
+    console.log(i);
+    i++;
+}
+
+let j = 0;
+
+do {
+    console.log(j);
+    j++;
+} while (j < 10);
+
+let count = 100;
+
+while (count >= 0) {
+    console.log(count);
+    count -= 10;
+}
+
+let backCount = 100;
+
+do {
+    console.log(backCount);
+    backCount -= 10;
+} while (backCount >= 0);
